f9f31d113b4f81d468bfeb1c541f929fe1e2e61b
Aaron <awoo452@wgu.edu>
2021-06-18 09:25:13 -0700

    fixing contact form

--- mail.php
+++ mail.php
@@ -1,16 +1,10 @@
-<?php
-if(isset( $_POST['name']))
-$name = $_POST['name'];
-if(isset( $_POST['email']))
+<?php $name = $_POST['name'];
 $email = $_POST['email'];
-if(isset( $_POST['message']))
 $message = $_POST['message'];
-if(isset( $_POST['subject']))
-$subject = $_POST['subject'];
-
-$content="From: $name \n Email: $email \n Message: $message";
+$formcontent="From: $name \n Message: $message";
 $recipient = "jaaronsservices@gmail.com";
+$subject = "Contact Form";
 $mailheader = "From: $email \r\n";
-mail($recipient, $subject, $content, $mailheader) or die("Error!");
-echo "Email sent!";
+mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+echo "Thank You!";
 ?>
